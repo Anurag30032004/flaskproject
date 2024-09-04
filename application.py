72228e55d9474a2628ecde0c17838eb834ab7480
Anurag paul <anuragpaul30032004@gmail.com>
2024-09-04 18:15:03 +0530

    init

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
-from flask import Flask
-application = Flask(__name__)
+from flask import Flask, render_template, request, flash # type: ignore
 
-@application.route('/')
-def hello_world():
-    return 'Hello, Everyone!!!'
+application = Flask(__name__)
+application.secret_key = "manbearpig_MUDMAN888"
+
+@application.route("/")
+def index():
+	flash("what's your name?")
+	return render_template("index.html")
+
+@application.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	return render_template("index.html")
